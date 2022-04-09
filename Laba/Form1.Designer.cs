d269ad9a3d32af71c01eb50d231b910fce68ddd5
gmaron <gmaron725>
2022-04-09 22:06:22 +0400

    Добавьте файлы проекта.

---
+++ Laba/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace Laba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 30);
+            this.listBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(307, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(307, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 30);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(560, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 43);
+            this.listBox3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Сортировать 1п";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(479, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Вывод";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(150, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Сорт в 2п с семафором";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 143);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Laba/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//https://gist.github.com/pivo5551/0e03683553c7903305f5bef0bd7a3ba6
+namespace Laba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int[] GenArray(int len)
+        {
+            Random random = new Random();
+            int[] arr = new int[len];
+            for (int i=0; i<len; i++)
+            {
+                arr[i] = random.Next(9);
+            }
+            return arr;
+        }
+        //для сортировки одним потоком
+        public static void ViborSortMax(int[] mas)
+        {
+            for (int i = mas.Length - 1; i >= 1; i--)
+            {
+                int max = i;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (mas[max] < mas[j])
+                    {
+                        max = j;
+                    }
+                }
+                int temp = mas[i];
+                mas[i] = mas[max];
+                mas[max] = temp;
+            }
+        }
+
+        //пузырька для многопоточной(Без Lock)
+        public static int[] BubbleSort(int[] mas)
+        {
+            int temp;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[i] > mas[j])
+                    {
+                        temp = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = temp;
+                    }
+                    Thread.Sleep(50);
+                }
+            }
+            return mas;
+        }
+        //выбора для многопоточной(Без Lock)
+        public static int[] ViborSort(int[] mas)
+        {
+
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                //поиск минимального числа
+                int min = i;
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[j] < mas[min])
+                    {
+                        min = j;
+                    }
+                    Thread.Sleep(50);
+                }
+                //обмен элементов
+                int temp = mas[min];
+                mas[min] = mas[i];
+                mas[i] = temp;
+            }
+            return mas;
+        }
+
+
+
+
+
+
+
+
+
+        private static readonly object lockObj = new object();
+        //пузырька для многопоточной(C Lock)
+        public static int[] BubbleSort_L(int[] mas)
+        {
+            int temp;
+            lock (lockObj)
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (mas[i] > mas[j])
+                        {
+                            temp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = temp;
+                        }
+                        Thread.Sleep(50);
+                    }
+                }
+            }
+            return mas;
+        }
+        //выбора для многопоточной(C Lock)
+        public static int[] ViborSort_L(int[] mas)
+        {
+            lock (lockObj)
+            { 
+                for (int i = 0; i < mas.Length - 1; i++)
+                {
+                    //поиск минимального числа
+                    int min = i;
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (mas[j] < mas[min])
+                        {
+                            min = j;
+                        }
+                        Thread.Sleep(50);
+                    }
+                    //обмен элементов
+                    int temp = mas[min];
+                    mas[min] = mas[i];
+                    mas[i] = temp;
+                }
+            }
+            return mas;
+        }
+
+
+
+        public static string ArrayToString(int[] mas)
+        {
+            string result = string.Empty;
+            for(int i=0; i< mas.Length; i++)
+            {
+                result += mas[i] + " ";
+            }
+            return result;
+        }
+
+        public static int[] array;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            array = GenArray(10);
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add(ArrayToString(array));
+        }
+
+        //Generate
+        private void button1_Click(object sender, EventArgs e)
+        {
+            array = GenArray(10);
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox1.Items.Add(ArrayToString(array));
+        }
+
+        //Проверить их работоспособность в обычном, (без поточном)  режиме.
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //BubbleSort(array);
+            //ViborSort(array);
+            ViborSortMax(array);
+
+            listBox2.Items.Clear();
+            listBox2.Items.Add(ArrayToString(array));
+            //----------------
+        }
+
+        //Debug
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            listBox3.Items.Add(ArrayToString(array));
+        }
+
+
+        //maximumCount = сколько потоков могут войти в крит секцию (выполнять метод)
+        //Счетчик для семафора уменьшается каждый раз, когда поток входит в семафор,
+        //и увеличивается, когда поток освобождает семафор. 0==блокировка до освобождения семафора другими потоками
+        public static Semaphore semaphoreObject = new Semaphore(initialCount: 2, maximumCount: 2);
+
+        public static Semaphore semaphoreObject1 = new Semaphore(initialCount: 1, maximumCount: 1);
+        public static Semaphore semaphoreObject2 = new Semaphore(initialCount: 1, maximumCount: 1);
+
+        //Сортировать в 2 потока с семафором
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (checkBox1.Checked)//lock on
+            {
+                listBox2.Items.Clear();
+
+                Thread thread1 = new Thread(() =>
+                {
+                    semaphoreObject.WaitOne();
+                    BubbleSort_L(array);
+                    semaphoreObject.Release();
+                });
+
+                Thread thread2 = new Thread(() =>
+                {
+                    semaphoreObject.WaitOne();
+                    ViborSort_L(array);
+                    semaphoreObject.Release();
+                });
+
+                thread1.Start(); thread2.Start();
+
+                thread1.Join(); thread2.Join();
+                listBox2.Items.Add(ArrayToString(array));
+            }
+            else //lockoff
+            {
+                listBox2.Items.Clear();
+
+                Thread thread1 = new Thread(() =>
+                {
+                    semaphoreObject1.WaitOne();
+                    BubbleSort(array);
+                    semaphoreObject1.Release();
+                });
+
+                Thread thread2 = new Thread(() =>
+                {
+                    semaphoreObject2.WaitOne();
+                    ViborSort(array);
+                    semaphoreObject2.Release();
+                });
+
+                thread1.Start(); thread2.Start();
+
+                thread1.Join(); thread2.Join();
+                listBox2.Items.Add(ArrayToString(array));
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
